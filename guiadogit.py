1db5b46c73e6ef1f50622e74ab09cf732ac3ea0a
Gabriel-2040 <132762837+Gabriel-2040@users.noreply.github.com>
2023-11-05 20:17:20 -0300

    Update guiadogit.py

--- guiadogit.py
+++ guiadogit.py
@@ -21,8 +21,9 @@
 # git restore --staged nome_do_arquivo ,  faz com que o arquivo ao inves de voltar ao que foi feito antes só 
 # volta da area de staged para a area de changes #
 # 
+# git remote - visualiza o local do arquivo onde ele se encontra
 # git push - vem apos o commit. ele envia tudo para o repositorio online do github 
-# 
+# git push "origin que é o repositorio remoto" e o "main" ou "master" que é o nosso local. 
 # git push  - ele traz modiicações feitas no github online para o seu arquivo e "merge" tudo junto.
 # 
 # #
