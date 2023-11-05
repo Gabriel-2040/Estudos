ffac50a8b412311800a6ceaae554d30aa35f65c8
Gabriel-2040 <132762837+Gabriel-2040@users.noreply.github.com>
2023-11-05 20:24:25 -0300

    Update guiadogit.py

--- guiadogit.py
+++ guiadogit.py
@@ -8,7 +8,7 @@
 # adionada nova linha
 # git status verifica o status do arquivo
 # fica na barra de preparado
-# git add . nome do arquiv ( pressioana tab para autocompletar) ( coloca o arquivo na area de stage)
+# git add .\ nome do arquivo ( pressioana tab para autocompletar) ( coloca o arquivo na area de stage)
 # adiciona no repositorio a modificação.
 # git commit -m "comentario suscinto do que foi feito"
 #
@@ -21,6 +21,7 @@
 # git restore --staged nome_do_arquivo ,  faz com que o arquivo ao inves de voltar ao que foi feito antes só 
 # volta da area de staged para a area de changes #
 # 
+<<<<<<< Updated upstream
 # git remote - visualiza o local do arquivo onde ele se encontra
 # git push - vem apos o commit. ele envia tudo para o repositorio online do github 
 # git push "origin que é o repositorio remoto" e o "main" ou "master" que é o nosso local. 
@@ -28,3 +29,10 @@
 # 
 # git fetch - ele diferente do git pull ele traz a informação antes. dai voc pode usar o git diff para ver a diferenças
 # e depois usar o git pull pra juntar os arquivos caso esteja coerente.
+=======
+# git remote - antes para acessar o repositorio remoto
+# git push - vem apos o commit. ele envia tudo para o repositorio online do github 
+
+# git pull  - ele traz modiicações feitas no github online para o seu arquivo e "merge" tudo junto.
+# 
+>>>>>>> Stashed changes
